cfccfe1f6f7350ebe7b79cc2a8b63c4170112960
Mykyta Makarov <mykyta.makarov@ericsson.com>
2016-12-21 15:06:48 +0100

    "auto commit model.pkl"

--- train.py
+++ train.py
@@ -1,10 +1,18 @@
 import preprocess
 from rnnnumpy import RNNNumpy
 import numpy as np
+import pickle
+
 dat = []
 data = preprocess.dataset()
 rnn = RNNNumpy(data.vocabulary_size)
-x = data.X_train[10]
+x = data.X_train[20006]
+
+np.random.seed(10)
+# Train on a small subset of the data to see what happens
+
+losses = RNNNumpy.train_with_sgd(rnn, data.X_train[:20000], data.Y_train[:20000], nepoch=10, evaluate_loss_after=1)
+
 predict = rnn.predict(x)
 
 print("predict shape = " + str(predict.shape))
@@ -12,5 +20,7 @@ print (unicode(predict))
 array_of_words = " ".join([data.index_to_word[x] for x in predict ])
 
 print( array_of_words)
-print "Expected Loss for random predictions: %f" % np.log(data.vocabulary_size)
-print "Actual loss: %f" % rnn.calculate_loss(data.X_train[:1000], data.Y_train[:1000])
+
+
+with open('model.pkl','wb') as out:
+    pickle.dump(rnn, out, pickle.HIGHEST_PROTOCOL)
